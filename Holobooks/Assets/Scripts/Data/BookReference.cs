d9f3cecce906277546063a3b40310641a112fa22
Ben Delaporte <bdelapor@illinois.edu>
2016-11-14 03:17:37 -0600

    First versions added for:
    BookData
    BookReference
    PageData
    
    and a Moby Dick public domain text asset added, as well.

---
+++ Holobooks/Assets/Scripts/Data/BookReference.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BookReference
+{
+	public string title;
+	public string author;
+	public int ISBN;
+	public List<int> visitedPages = new ArrayList<int> ();
+	public int currentPage = 0;
+
+
+	public BookReference (string newTitle)
+	{
+		this.title = newTitle;
+	}
+
+}
