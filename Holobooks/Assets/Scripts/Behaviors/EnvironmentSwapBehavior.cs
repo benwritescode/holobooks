d63f077775a9f4d90dbb6e7bd079635cd2e49f4f
Ben Delaporte <bdelapor@illinois.edu>
2016-12-08 08:37:44 -0600

    First import of EnvironmentSwapGameObject in scene, and the new EnvironmentSwapBehavior (which should be added as a component of EnvironmentSwapGameObject once compile errors are resolved.)

---
+++ Holobooks/Assets/Scripts/Behaviors/EnvironmentSwapBehavior.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnvironmentSwapBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
