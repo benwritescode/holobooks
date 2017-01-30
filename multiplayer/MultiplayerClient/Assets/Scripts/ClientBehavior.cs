728ac7717529defe438b4e769744e4e86c6331e0
Ben Delaporte <bdelapor@illinois.edu>
2017-01-30 05:42:05 -0600

    Multiplayer demo/testing. Very early progress. No working network communication yet, but a lot of groundwork laid for network communication, and for easy maintanence of the server project.

---
+++ multiplayer/MultiplayerClient/Assets/Scripts/ClientBehavior.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net.Sockets;
+using System.IO;
+using System.Text;
+
+public class ClientBehavior : MonoBehaviour
+{
+	// https://msdn.microsoft.com/en-us/library/system.net.sockets.tcpclient(v=vs.110).aspx
+
+	const int port = 5555;
+	const string host_ip = "127.0.0.1";
+
+	TcpClient client;
+
+	//https://msdn.microsoft.com/en-us/library/system.io.streamreader(v=vs.110).aspx
+	StreamWriter writer;
+
+
+	//https://msdn.microsoft.com/en-us/library/system.io.streamwriter(v=vs.110).aspx
+	StreamReader reader;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+		client = new TcpClient (host_ip, port);
+		writer = StreamWriter (client.GetStream (), Encoding.ASCII);
+		reader = StreamReader (client.GetStream (), Encoding.ASCII);
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		string my_message;
+		while (my_message = reader.ReadLine ()) {
+			Debug.Log ("Received message: " + my_message);
+		}
+
+	}
+}
---
+++ multiplayer/server/server.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Net.Sockets;
+using System.IO;
+using System.Text;
+
+class Server {  
+
+	const int port = 5555;
+	const string server_ip = "127.0.0.1";
+
+	static void Main() {
+		Console.WriteLine("\n============\n\nHolobooks server starting");
+
+		IPAddress localAdd = IPAddress.Parse(SERVER_IP);
+
+			
+	}
+
+
+}
