f344bf8e29af50460bfe04d526cfe9098071bd50
Ben Delaporte <bdelapor@illinois.edu>
2017-02-06 02:50:28 -0600

    Client connects to server successfully.
    Still need to do some work to make the client capable of listening for messages without freezing the GUI thread. Unfortunately, Unity doesn't support StreamReader.AsyncReadLine. It only supports ReadLine. The only workaround to this is to create a worker thread for the stream reader, then save all lines into a buffer of some kind, and the buffer must be locked. Then, when our ClientBehavior script updates, it will attempt to get the lock on the buffer, and one by one consume the buffer's lines with a delegate callback function. (LineRead(String str...)) Not sure whether ClientBehavior should read strings and call delegate, or read strings, parse JSON, and call delegate. Probably would be better to have a new AsyncStreamReader class, and have it call a delegate, and then ClientBehavior can be the delegate and consume the strings from JSON into objects, and then call a delegate function with those objects.

--- multiplayer/MultiplayerClient/Assets/Scripts/ClientBehavior.cs
+++ multiplayer/MultiplayerClient/Assets/Scripts/ClientBehavior.cs
@@ -16,27 +16,41 @@ public class ClientBehavior : MonoBehaviour
 	//https://msdn.microsoft.com/en-us/library/system.io.streamreader(v=vs.110).aspx
 	StreamWriter writer;
 
-
 	//https://msdn.microsoft.com/en-us/library/system.io.streamwriter(v=vs.110).aspx
 	StreamReader reader;
 
 	// Use this for initialization
 	void Start ()
 	{
+		Debug.Log ("ClientBehavior: start()");
+		try {
+			
+			client = new TcpClient ("localhost", port);
+			writer = new StreamWriter (client.GetStream (), Encoding.ASCII);
+			reader = new StreamReader (client.GetStream (), Encoding.ASCII);
 
-		client = new TcpClient (host_ip, port);
-		writer = StreamWriter (client.GetStream (), Encoding.ASCII);
-		reader = StreamReader (client.GetStream (), Encoding.ASCII);
+			if (client != null)
+				Debug.Log ("Connected to server!");
+
+		} catch (System.Net.Sockets.SocketException e) {
+			Debug.Log ("ClientBehavior: unable to connect to server socket.");
+		}
 
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		string my_message;
-		while (my_message = reader.ReadLine ()) {
-			Debug.Log ("Received message: " + my_message);
+		if (reader != null) {
+			string my_message = reader.ReadLineAsync ();
+			while (null != my_message) {
+				Debug.Log ("Received message: " + my_message);
+
+				my_message = null;
+				my_message = reader.ReadLine ();
+			}
 		}
+
 
 	}
 }
