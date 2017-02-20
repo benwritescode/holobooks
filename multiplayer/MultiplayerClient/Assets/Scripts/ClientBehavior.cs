25d28be559ffd75ce23aae7de791d8bd0c8d9f11
Ben Delaporte <bdelapor@illinois.edu>
2017-02-19 19:12:09 -0600

    Multithreaded server working, and acting as a forwarding station to forward messages between clients. For more documentation, see the Trello board

--- multiplayer/MultiplayerClient/Assets/Scripts/ClientBehavior.cs
+++ multiplayer/MultiplayerClient/Assets/Scripts/ClientBehavior.cs
@@ -6,9 +6,16 @@ using System.Text;
 
 public class ClientBehavior : MonoBehaviour
 {
+
+	// TODO:
+	// unfortunately, Unity doesn't support StreamReader.AsyncReadLine()
+	// instead, we will have to make a thread to consume packets asynchronously.
+	// need to make a thread to read lines, a buffer to store them,
+	// and then within update, we should call a delegate function whenever we consume a string from the buffer.
+
 	// https://msdn.microsoft.com/en-us/library/system.net.sockets.tcpclient(v=vs.110).aspx
 
-	const int port = 5555;
+	const int port = 5556;
 	const string host_ip = "127.0.0.1";
 
 	TcpClient client;
@@ -29,8 +36,20 @@ public class ClientBehavior : MonoBehaviour
 			writer = new StreamWriter (client.GetStream (), Encoding.ASCII);
 			reader = new StreamReader (client.GetStream (), Encoding.ASCII);
 
+
+
 			if (client != null)
 				Debug.Log ("Connected to server!");
+			Debug.Log ("ClientBehavior: start(): writing message");
+
+
+			writer.WriteLine ("Client says \"Hello there!\"");
+			writer.Flush ();
+			Debug.Log ("ClientBehavior: start(): wrote message");
+
+
+			Debug.Log ("Received message back: " + reader.ReadLine ());
+
 
 		} catch (System.Net.Sockets.SocketException e) {
 			Debug.Log ("ClientBehavior: unable to connect to server socket.");
@@ -41,16 +60,16 @@ public class ClientBehavior : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		if (reader != null) {
-			string my_message = reader.ReadLineAsync ();
-			while (null != my_message) {
-				Debug.Log ("Received message: " + my_message);
-
-				my_message = null;
-				my_message = reader.ReadLine ();
-			}
-		}
-
+//		if (reader != null) {
+//			string my_message = reader.ReadLine ();
+//			while (null != my_message) {
+//				Debug.Log ("Received message: " + my_message);
+//
+//				my_message = null;
+//				my_message = reader.ReadLine ();
+//			}
+//		}
+//
 
 	}
 }
--- multiplayer/server/server/Program.cs
+++ multiplayer/server/server/Program.cs
@@ -3,9 +3,23 @@
 // http://mono.1490590.n4.nabble.com/quot-System-Net-not-found-quot-using-monodevelop-td1508820.html
 
 // Mutex stuff: http://www.martyndavis.com/?p=440
+// Good threading tutorial: http://www.mono-project.com/archived/threadsbeginnersguide/
+// More on threading: http://www.mono-project.com/docs/tools+libraries/tools/gendarme/rules/concurrency/
+
+// Tutorial on "waiting" for a signal in C#. This is useful when we are waiting for new messages to forward to other clients.
+
+// Good tutorial on locking:
+
+// Useful for TCP errors: https://msdn.microsoft.com/en-us/library/system.net.sockets.tcplistener.accepttcpclient(v=vs.110).aspx
+
+// Useful tip for passing arguments to a new thread (by using a lambda function): 
+// http://stackoverflow.com/questions/3360555/how-to-pass-parameters-to-threadstart-method-in-thread
 
 using System;
 using System.Collections;
+
+// For List
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 
@@ -17,77 +31,60 @@ using System.Threading;
 
 
 
+
 class Server
 {
 
-	const int port = 5555;
+	const int port = 5556;
 	const string my_ip = "127.0.0.1";
-	Object lockMessageQueue = new Object ();
 
 
-	Object lockStreamWriters = new object ();
-	//	//	List<StreamWriter> streamWriters = new List<StreamWriter> ();
+	Object lockMessages = new Object ();
+	private Queue<string> messagesQueue = new Queue<string> ();
 
+	Object lockStreamWriters = new Object ();
+	private List<StreamWriter> streamWriters = new List<StreamWriter> ();
+
+	AutoResetEvent waitForNewMessages = new AutoResetEvent (false);
 
 
 	public static void Main (string[] arg)
 	{
 		Console.WriteLine ("\n============\n\nHolobooks server starting\n============\n\n");
 
-
-
-		Server myStartThreads = new Server ();
-
-
-
-		// start listening for clients.
-
-
-
-
-//		Console.WriteLine ("Client connected! Listening for messages.");
-//
-//		writer.WriteLine ("Hello client!");
-//
-//		while (reader != null) {
-//			string my_message = reader.ReadLine ();
-//			if (null != my_message) {
-//				Console.WriteLine ("Received message: " + my_message);
-//				writer.WriteLine ("You sent: " + my_message); 
-//			}
-//		}
-
-
-
-			
+		// Initializing the server also starts listening for new clients.
+		Server myServer = new Server ();
+					
 	}
 
 
 	public Server ()
 	{
-		// initialize all my threads here.
-		Thread listen = new Thread (new ThreadStart (this.ListenForClients));
+		// initialize first threads here.
+		Thread waitForNewClients = new Thread (new ThreadStart (this.WaitForNewClients));
+		Thread writeMessagesToClients = new Thread (new ThreadStart (this.WriteMessagesToClients));
 
-		listen.Start ();
+		waitForNewClients.Start ();
+		writeMessagesToClients.Start ();
 
-		// wait for listen
-		listen.Join ();
+
 	}
 
-	public void ListenForClients ()
+	public void WaitForNewClients ()
 	{
 
-		Console.WriteLine ("server: ListenForClients()\n");
-
+		Console.WriteLine ("WaitForNewClients:\n");
 		System.Net.IPAddress localAddress = IPAddress.Parse (my_ip);
 
 		TcpListener tcpListener = new TcpListener (localAddress, port);
+		tcpListener.Start ();
 
 		while (tcpListener != null) {
+			Console.WriteLine ("WaitForNewClients: Listening for more clients...\n");
+
 			TcpClient client = tcpListener.AcceptTcpClient ();
 			if (client != null) {
-				Console.WriteLine ("New client connected!");
-
+				Console.WriteLine ("WaitForNewClients: New client connected!");
 
 
 				StreamWriter writer = new StreamWriter (client.GetStream (), Encoding.ASCII);
@@ -95,20 +92,112 @@ class Server
 
 				StreamReader reader = new StreamReader (client.GetStream (), Encoding.ASCII);
 				lock (lockStreamWriters) {
-
-					this.streamWriters.Insert (writer);
+					this.streamWriters.Add (writer);
 				}
-
+				// Start a new thread to listen to a client.
+				Thread newClientListenThread = new Thread (new ThreadStart (() => this.ListenToClient (reader)));
+				newClientListenThread.Start ();
 				
 			}
 		}
-			
+	}
 
+	public void WriteMessagesToClients ()
+	{
+
+
+		Queue<StreamWriter> writersToRemove = new Queue<StreamWriter> ();
+
+
+		while (true) {
+			// Wait for new messages before attempting to lock again.
+			waitForNewMessages.WaitOne ();
+			Console.WriteLine ("WriteMessagesToClients: received signal. Will attempt to write message.");
+
+
+			// Obtain a lock on the stream writers
+			// so that we can write a message
+			// Note: we are locking two locks here.
+			// But since we are the only thread locking two locks, we don't have to worry about deadlock.
+			// If another thread ever does need to lock two locks, just make sure they acquire the locks in the same order as this thread.
+
+			// ENTER critical section
+			lock (lockStreamWriters) {
+				lock (lockMessages) {
+					// Once we obtain the lock, write the message.
+					while (messagesQueue.Count > 0) {
+
+						string message = messagesQueue.Dequeue ();
+
+						foreach (StreamWriter mywriter in this.streamWriters) {
+
+
+							// Make sure the writer is still good. If it's not, we'll remove it.
+							if (mywriter.BaseStream != null) {
+
+								try {
+									Console.WriteLine ("WriteMessagesToClients: writing message: " + message);
+
+									mywriter.WriteLine ("Server says \"Hello!\" You sent: \"" + message + "\""); 
+
+									// Flushing aggressively for now. 
+									// This likely shouldn't cause performance issues, since our messages will have a relatively slow rate, anyway
+
+									mywriter.Flush ();
+								} catch (System.IO.IOException e) {
+									writersToRemove.Enqueue (mywriter);
+								}
+							} else {
+								writersToRemove.Enqueue (mywriter);
+							}
+						}
+					}
+				}
+
+				// Released lock on messages
+				// But still have lock on stream writers list
+				// Let's remove all the defunct stream writers with bad sockets now.
+				// (we couldn't remove them earlier, because you can't alter a list while "foreach" looping through it.)
+				while (writersToRemove.Count > 0) {
+					Console.WriteLine ("WriteMessagesToClients: Client disconnected while attempting to write a message.");
+					StreamWriter toRemove = writersToRemove.Dequeue ();
+					this.streamWriters.Remove (toRemove);
+
+				}
+			}
+			// LEAVE critical section
+
+
+		}
 
 	}
 
 	public void ListenToClient (StreamReader reader)
 	{
+		Console.WriteLine ("ListenToClient: Client connected! Listening for messages.");
+
+		while (reader.BaseStream != null) {
+			try {
+				string my_message = reader.ReadLine ();
+				if (null != my_message) {
+					Console.WriteLine ("ListenToClient: Received message: " + my_message);
+
+					// Obtain a lock on the messages queue
+					// so that we can save the new message into the queue.
+					lock (lockMessages) {
+						// Once we obtain the lock, save the message in the messages queue.
+						messagesQueue.Enqueue (my_message);
+					}
+
+					// Notify the WriteMessagesToClients thread that a new message is waiting to be written.
+					waitForNewMessages.Set ();
+				}
+			} catch (System.IO.IOException e) {
+				Console.WriteLine ("ListenToClient: Client disconnected during readLine().");
+			}
+		}
+
+		Console.WriteLine ("ListenToClient: Client disconnected.");
 
 
 	}
