83f1ddddd2d06da82454a0d938e4f2cb8cf7f3d2
Ben Delaporte <bdelapor@illinois.edu>
2017-03-17 09:16:58 -0500

    SpeechTest working. Open SpeechTest Unity project, and run it. Then, in the scene, press and hold the spacebar while you speak something. When you release the spacebar, your voice is saved as a .wav file. Then, Unity starts a Python script, which uses PocketSphinx to convert your speech into text, and returns the text as output. This output is piped back into Unity. Then a callback is called with your speech/text, which is then printed to the console via Debug.Log. The library we are using (SpeechRecognition) in Python can use various backends. It's currently using PocketSphinx, because PocketSphinx can be used offline. However, Google Speech API is another optional backend for SpeechRecognition, and it's not difficult to swap out the backend.

---
+++ speech/SpeechTest/Assets/scripts/SpeechToText.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+// For threads
+using System.Threading;
+
+// For processes
+using System.Diagnostics;
+using System;
+
+
+// get OS
+// https://docs.unity3d.com/ScriptReference/SystemInfo-operatingSystem.html
+
+// for getting path of Python
+using Configuration;
+
+public class SpeechToText : MonoBehaviour
+{
+
+
+	// See Microsoft tutorial for delegates/callbacks here:
+	// Here is the delegate type for returning the text result.
+	public delegate void SpeechToTextCallback (string text);
+
+
+	// Use this for initialization
+	void Start ()
+	{
+
+
+
+	}
+
+	public void ConvertClipToTextWithCallback (AudioClip clip, SpeechToTextCallback callback)
+	{
+
+		string dataPath = Application.dataPath;
+		string pythonPath = Config.PythonPath ();
+
+
+		// save the audio clip as a .wav file
+		SavWav.Save (dataPath, "clip", clip);
+
+
+		// Start a new thread to translate the speech into text
+		// We have to call an outside process. Since the process can require indeterminate time, 
+		// we don't want to wait for it on the main thread (which would cause a graphical freeze)
+		// instead, we offload this work to a new thread.
+		// when that new thread is completed, it will call the client's provided callback, along with the recognized speech.
+		Thread newThread = new Thread (new ThreadStart (() => this.SpeechToTextThread (clip, callback, dataPath, pythonPath)));
+		newThread.Start ();
+
+	}
+		
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+
+	void SpeechToTextThread (AudioClip clip, SpeechToTextCallback callback, string dataPath, string pythonPath)
+	{
+		Process process = new Process ();
+
+		process.StartInfo.CreateNoWindow = true;
+
+		process.StartInfo.UseShellExecute = false;
+
+		process.StartInfo.RedirectStandardOutput = true;
+
+		process.StartInfo.FileName = pythonPath;
+
+		process.StartInfo.Arguments = "convert_speech.py";
+
+		process.StartInfo.WorkingDirectory = dataPath;
+
+
+		int code = -2;
+		string output = "";
+
+		try {
+			process.Start ();
+
+			output = process.StandardOutput.ReadToEnd ();
+
+			// wait until completion, and then call the callback
+			process.WaitForExit ();
+
+
+		} catch (Exception e) {
+
+			UnityEngine.Debug.Log ("SpeechToTextThread: conversion failed: " + e.ToString ());
+
+
+		} finally {
+			code = process.ExitCode;
+			process.Dispose ();
+			process = null;
+
+			callback ("" + output);
+
+		}
+
+
+	}
+
+}
---
+++ speech/SpeechTest/Assets/scripts/SpeechToTextTester.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class SpeechToTextTester : MonoBehaviour
+{
+
+	SpeechToText mySpeechToText;
+
+	AudioClip clip;
+
+	// Use this for initialization
+	void Start ()
+	{
+		mySpeechToText = GameObject.FindObjectOfType<SpeechToText> ();
+
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+
+
+		if (Input.GetKeyDown ("space")) {
+//			Debug.Log ("Start recording");
+			clip = Microphone.Start (null, true, 60, 44100);
+
+		}
+
+		if (Input.GetKeyUp ("space")) {
+
+
+			Microphone.End (null);
+//			Debug.Log ("End recording");
+			mySpeechToText.ConvertClipToTextWithCallback (clip, this.RecognizedText);
+
+		}
+	}
+
+
+
+	void RecognizedText (string text)
+	{
+
+		Debug.Log ("SpeechToText output: " + text);
+
+	}
+}
