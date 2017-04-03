858c806b404f58a1be271b046fadf71890264c98
Ben Delaporte <bdelapor@illinois.edu>
2017-04-03 06:43:08 -0500

    new method to install murmur dependencies

---
+++ multiplayer/server/install_murmur_dependencies.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+if [[ $EUID > 0 ]]; then 
+	echo "Please run make one time as root to install necessary dependencies."; 
+	return 1;
+else \
+
+	# dependencies
+
+	sudo apt-get install build-essential pkg-config qt5-default qtbase5-dev qttools5-dev qttools5-dev-tools libqt5svg5* \
+	libspeex1 libspeex-dev libboost-dev libasound2-dev libssl-dev g++ \
+	libspeechd-dev libzeroc-ice-dev ice-slice libpulse-dev slice2cpp \
+	libcap-dev libspeexdsp-dev libprotobuf-dev protobuf-compiler \
+	libogg-dev libavahi-compat-libdnssd-dev libsndfile1-dev \
+	libg15daemon-client-dev libxi-dev 
+	sudo apt-get install git
+	sudo apt-get install zip	
+	wget https://github.com/mumble-voip/mumble/archive/master.zip
+	unzip master.zip -d mumble
+
+	# checkout stable 1.2.x ...
+	cd mumble/mumble-master \
+	&& git submodule init \
+	&& git submodule update \
+	&& git pull
+
+	# && git checkout --track -b 1.2.x origin/v1.2.x \
+
+	
+
+	touch .murmur_dependencies;
+fi; 
