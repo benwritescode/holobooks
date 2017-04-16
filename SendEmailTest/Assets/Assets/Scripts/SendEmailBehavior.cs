6cf7ac8aa817308d3c901a80c3e46e2df428d37d
Jaytu72 <jaytu72@gmail.com>
2017-04-16 13:44:52 -0500

    sendemail function
    first commit

--- SendEmailTest/Assets/Assets/Scripts/SendEmailBehavior.cs
+++ SendEmailTest/Assets/Assets/Scripts/SendEmailBehavior.cs
@@ -2,18 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+
 using Configuration;
 
 
 public class SendEmailBehavior : MonoBehaviour
 {
-
+	//static bool mailSent = false;
 	// Use this for initialization
 	void Start ()
 	{
+		//ServicePointManager.ServerCertificateValidationCallback = 
+		//	delegate(object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) {
+		//	return true;
+		//};
+
+
+		//string server = "smtp.gmail.com";
 		
+		//moved these up here after reading the link you sent me
+		//string username = Config.GetString("gmail_username"); 
+		//string password = Config.GetString("gmail_password");
+
+		//SmtpClient client = new SmtpClient
+							//{
+							//	Host = server,
+							//	Port = 587,
+							//	EnableSsl = true,
+							//	DeliveryMethod = SmtpDeliveryMethod.Network,
+							//	UseDefaultCredentials = false,
+							//	Credentials = new System.Net.NetWorkCredential(username,password)
+							//};
 	}
-	
+
 	// Update is called once per frame
 	void Update ()
 	{
@@ -29,6 +54,24 @@ public class SendEmailBehavior : MonoBehaviour
 		string username = Config.GetString ("gmail_username");
 		string password = Config.GetString ("gmail_password");
 
+		MailAddress fro = new MailAddress (username);
+		MailAddress to = new MailAddress (toAddress);
+
+		MailMessage mail = new MailMessage(fro,to);
+		mail.Subject = "Subject";
+		mail.Body = message;
+
+		SmtpClient client = new SmtpClient();
+
+		client.Host = "smtp.gmail.com";
+		client.Port = 587;
+		client.EnableSsl = true;
+		client.DeliveryMethod = SmtpDeliveryMethod.Network;
+		client.UseDefaultCredentials = false;
+
+		client.Credentials = (ICredentialsByHost)new NetworkCredential(username, password);
+		
+		client.Send(mail);
 
 		// use C# APIs to send an email from the user’s provided gmail username.
 
