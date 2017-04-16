48f3c04637076475aa00f54af47b40a3af2ba82b
Ben Delaporte <bdelapor@illinois.edu>
2017-04-15 20:43:10 -0500

    SendEmailTest project stubbed out

---
+++ SendEmailTest/Assets/Assets/Scripts/SendEmailBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Configuration;
+
+
+public class SendEmailBehavior : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+
+	public void SendEmail (string toAddress, string message)
+	{
+		// get username and password from config.cs
+		string username = Config.GetString ("gmail_username");
+		string password = Config.GetString ("gmail_password");
+
+
+		// use C# APIs to send an email from the user’s provided gmail username.
+
+	}
+}
