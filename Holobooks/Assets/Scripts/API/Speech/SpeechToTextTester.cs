7c37dd7dbdcbc6e0b92cbdc02d706190ec9ec890
Ben Delaporte <bdelapor@illinois.edu>
2017-04-26 14:39:33 -0500

    fixing SpeechToText so that it only calls caller's callback on the main thread. However, it's not currently converting the speech to text correctly; output is "". Committing so we can debug together.

--- Holobooks/Assets/Scripts/API/Speech/SpeechToTextTester.cs
+++ Holobooks/Assets/Scripts/API/Speech/SpeechToTextTester.cs
@@ -9,10 +9,20 @@ public class SpeechToTextTester : MonoBehaviour
 	SpeechToText mySpeechToText;
 	AudioClip clip;
 
+	System.Threading.Thread mainThread;
+
+
+
 	// Use this for initialization
+	// Check to make sure callback is always called on mainthread:
+	// http://stackoverflow.com/questions/26452609/find-out-if-im-on-the-unity-thread
+
 	void Start ()
 	{
-		mySpeechToText = SpeechToText.instance;
+		mainThread = System.Threading.Thread.CurrentThread;
+
+		mySpeechToText = GameObject.FindObjectOfType<SpeechToText> (); 
+		//SpeechToText.instance;
 	}
 
 	// Update is called once per frame
@@ -36,6 +46,13 @@ public class SpeechToTextTester : MonoBehaviour
 	{
 
 		Debug.Log ("SpeechToText output: " + text);
+		Debug.Log ("Make sure we're on main thread by using MainThread only function: " + Application.dataPath);
+		if (mainThread.Equals (System.Threading.Thread.CurrentThread)) {
+			Debug.Log ("Yay, our callback was called on the main thread.");
 
+		} else {
+			Debug.LogWarning ("Uh oh, the callback wasn't called on the main thread.");
+		}
+			
 	}
 }
